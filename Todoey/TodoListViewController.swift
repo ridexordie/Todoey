9c5fdf5a726b7e69d57ab44af6a761f1eedf3a0e
tunc <tunc.akkoyun@netix.com.tr>
2018-01-21 21:33:44 +0300

    todo list with no data persistence.

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Tunc on 21.01.2018.
+//  Copyright © 2018 Tunc. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["Find Mike", "Call boss", "Buy eggs", "Repair car", "Paint the wall", "Buy 2 tickets to Iron Maiden", "Eat dinner"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    //MARK - Tableview datasource methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    
+    //MARK - Tableview delegate methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+}
+
